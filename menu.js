1add41a6db89d85bf4ea6a51ea8fc8e20f8385f2
marianpignani <marianogpignani@gmail.com>
2024-04-30 09:44:03 -0300

    media querys 2

---
+++ menu.js
@@ -0,0 +1,32 @@
+const navbar = document.querySelector("#navbar");
+const open = document.querySelector("#open");
+const closed = document.querySelector("#one");
+const closedT = document.querySelector("#two");
+const closedTh = document.querySelector("#three");
+const closedF = document.querySelector("#four");
+const closedFi = document.querySelector("#five");
+
+
+open.addEventListener("click", () => {
+    navbar.classList.add("visible");
+})
+
+closed.addEventListener("click", () => {
+    navbar.classList.remove("visible");
+})
+
+closedT.addEventListener("click", () => {
+    navbar.classList.remove("visible");
+})
+
+closedTh.addEventListener("click", () => {
+    navbar.classList.remove("visible");
+})
+
+closedF.addEventListener("click", () => {
+    navbar.classList.remove("visible");
+})
+
+closedFi.addEventListener("click", () => {
+    navbar.classList.remove("visible");
+})
